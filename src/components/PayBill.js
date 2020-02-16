77f187339f315b71cc5abd4ff72bea276c9d353e
Vibha Rana <ranavibha02@gmail.com>
2020-02-16 12:17:05 -0800

    comits added

--- src/components/PayBill.js
+++ src/components/PayBill.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import Modal from 'react-modal';
 
-
+//display the modal using isOpen. set the input as number and give minimum value as 0
 const PayBills  = (props) => {
     
     return (
         <Modal
         isOpen={props.open}
-        contentLabel = "Add payment"
+        paymentLabel = "Add payment"
         class="dialog"
         >
+
             
-       <h1>Pay The Bill</h1>
-       <h2>Amount To Pay: <input type="number" name="amountToPay" id="amountToPay" min="0" defaultValue="0"/></h2>
+       <h1>Pay The Bill</h1> 
+       <hr></hr>
+       <h2>Amount To Pay:<br></br> 
+       <input type="number" name="payAmount" id="payAmount" min="0" defaultValue="0"/></h2>
       <hr></hr>
       <br></br>
     
-       <button onClick={() => { props.balenceCalculation(document.getElementById("amountToPay").value) }}>Pay Bill</button>&nbsp;&nbsp;
+       <button onClick={() => { props.balenceCalculation(document.getElementById("payAmount").value) }}>Pay Bill</button>&nbsp;&nbsp;
        <button onClick={() => { props.balenceCalculation(0) }}>Cancel</button>
        <hr></hr>
         </Modal>
--- src/components/itemTable.js
+++ src/components/itemTable.js
@@ -5,20 +5,31 @@ class ItemTable extends React.Component {
     removeItem = (index) => { 
         this.props.onUpdateItem(index,0,true)
     }
+
+
+//render & assign the constant value a false. Use for each loop. If quantity >0, theItem = true. Else you will be sked to add some items 
     render() {
-        let hasItem = false
+        let theItem = false
         this.props.products.forEach(e => {
             if (e.quantity > 0) {
-                hasItem = true;
+                theItem = true;
             }
         });
-        if (!hasItem) {
+
+
+        //if there are no items ask to add some, Set a constant(sum) = 0  and finf the sum by increment the product of price and quantity
+        if (!theItem) {
             return <div>Please add some items</div>
         } else {
             let sum = 0
             this.props.products.forEach(e => {
                 sum += e.price * e.quantity
             });
+
+
+
+
+            //return the table 
             return (
                 <div>
 
@@ -33,6 +44,9 @@ class ItemTable extends React.Component {
                                 <th align="left">Remove Items</th>
                             </tr>
                         </thead>
+
+
+                        
                         <tbody>
                             {this.props.products.map((item, i) => {
 
@@ -52,7 +66,7 @@ class ItemTable extends React.Component {
                             )}
                             <tr class="row">
                                 <td colSpan="3" class="tableTotal">Total Money Spent:</td>
-                                
+                               
                                 <td>{sum.toLocaleString("en-US", { style: 'currency', currency: 'USD' })}</td>
                                 <td>Deleted Items</td>
                             </tr>
